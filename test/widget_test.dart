d3388199f8d27823272a228a5872fea3dca20a37
William <16344021+Yllib@users.noreply.github.com>
2026-08-24 23:00:42 -0400

    Remove placeholder app test

--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('App launches without error', (WidgetTester tester) async {
-    // Placeholder test
-    expect(true, isTrue);
-  });
-}
